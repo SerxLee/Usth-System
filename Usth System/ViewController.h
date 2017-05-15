c05443261d8c6075df23fb1f7cba840eabc9320c
serxlee <serx.lee@gmail.com>
2017-05-15 10:26:06 +0800

    Initial Commit

---
+++ Usth System/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Usth System
+//
+//  Created by Serx on 2017/5/15.
+//  Copyright © 2017年 Serx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Usth SystemTests/Usth_SystemTests.m
@@ -0,0 +1,39 @@
+//
+//  Usth_SystemTests.m
+//  Usth SystemTests
+//
+//  Created by Serx on 2017/5/15.
+//  Copyright © 2017年 Serx. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Usth_SystemTests : XCTestCase
+
+@end
+
+@implementation Usth_SystemTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
